d51c1fbffaf34bce69fbdc503db43c93828114a4
PraisePJ <93436815+PraisePJ@users.noreply.github.com>
2023-09-13 21:38:05 +0100

    Update settings.py

--- StudentManager/settings.py
+++ StudentManager/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'manager',
-    'psycopg2',
+]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
