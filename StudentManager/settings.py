8bd9bac7b8b6214c8d8b11cb2908fe16c8f0b9fc
PraisePJ <93436815+PraisePJ@users.noreply.github.com>
2023-09-13 15:36:11 +0100

    Update settings.py

--- StudentManager/settings.py
+++ StudentManager/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'StudentManager.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'studentmanager',
+        'NAME': 'postgres',
         'USER': 'student_manager_duy1_user',
         'PASSWORD': 'w1NF8A9ubLrWiFL7JxEZcQjl4DXBNc5j',
         'HOST': 'dpg-ck0mdgm3ktkc73fhp62g-a',
