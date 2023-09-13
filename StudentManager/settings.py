c56e46900f2f6b7cc32c4de80f058430329233a5
PraisePJ <93436815+PraisePJ@users.noreply.github.com>
2023-09-13 14:38:21 +0100

    Update settings.py

--- StudentManager/settings.py
+++ StudentManager/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'StudentManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'studentmanager',
+        'USER': 'student_manager_duy1_user',
+        'PASSWORD': 'w1NF8A9ubLrWiFL7JxEZcQjl4DXBNc5j',
+        'HOST': 'dpg-ck0mdgm3ktkc73fhp62g-a',
+        'PORT': '5432',
     }
 }
 
